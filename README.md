a671cbf644ec992bf09dd23a31df973dcfa9ee5b
BlurryFace1900 <48728786+BlurryFace1900@users.noreply.github.com>
2021-12-29 20:53:47 +0530

    final commit (probably)

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Random-Quote-Machine-React
 Created with CodeSandbox
-
-FreeCodeCamp FrontEnd Development 1st Certification Project Project
--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./styles.scss";
+import ReactFCCtest from "react-fcctest";
 import React, { useState, useEffect } from "react";
 import COLOR from "./colors.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faTumblr } from "@fortawesome/free-brands-svg-icons";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
-const element1 = <FontAwesomeIcon icon={faTwitter} />;
-const element2 = <FontAwesomeIcon icon={faTumblr} />;
-const element3 = <FontAwesomeIcon icon={faQuoteLeft} />;
+const twittericon = <FontAwesomeIcon icon={faTwitter} />;
+const tumblricon = <FontAwesomeIcon icon={faTumblr} />;
+const quoteicon = <FontAwesomeIcon icon={faQuoteLeft} />;
 let QuotesURL =
   "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 export default function App() {
-  const [quote, setQuote] = useState();
-  const [author, setAuthor] = useState();
+  const [quote, setQuote] = useState(
+    " When I was 5 years old, my mother always told me that happiness was the key to life. When I went to school, they asked me what I wanted to be when I grew up. I wrote down ‘happy’. They told me I didn’t understand the assignment, and I told them they didn’t understand life."
+  );
+  const [author, setAuthor] = useState("John Lennon");
   const [quotesArray, setQuotesArray] = useState(null);
   const [color, setColor] = useState("red");
 
@@ -38,39 +41,42 @@ export default function App() {
   document.body.style.backgroundColor = color;
 
   return (
-    <div className="App" style={{ backgroundColor: color }}>
-      <div id="quote-box" style={{ color: color }}>
-        <h3 id="text">
-          <span>{element3} </span>
-          {quote}
-        </h3>
-        <h4 id="author">- {author}</h4>
-        <a
-          style={{ backgroundColor: color }}
-          id="tweet-quote"
-          target="_blank"
-          href={encodeURI(
-            `http://www.twitter.com/intent/tweet?text=${quote} -${author}`
-          )}
-        >
-          {element1}
-        </a>
-        <a
-          style={{ backgroundColor: color }}
-          id="tumblr-quote"
-          target="_blank"
-          href={encodeURI(`http://www.tumblr.com/`)}
-        >
-          {element2}
-        </a>
-        <button
-          style={{ backgroundColor: color }}
-          id="new-quote"
-          onClick={() => getRandomQuotes()}
-        >
-          New Quote
-        </button>
+    <>
+      <ReactFCCtest />
+      <div className="App" style={{ backgroundColor: color }}>
+        <div id="quote-box" style={{ color: color }}>
+          <h3 id="text">
+            <span>{quoteicon} </span>
+            {quote}
+          </h3>
+          <h4 id="author">- {author}</h4>
+          <a
+            style={{ backgroundColor: color }}
+            id="tweet-quote"
+            target="_blank"
+            href={encodeURI(
+              `http://www.twitter.com/intent/tweet?text=${quote} -${author}`
+            )}
+          >
+            {twittericon}
+          </a>
+          <a
+            style={{ backgroundColor: color }}
+            id="tumblr-quote"
+            target="_blank"
+            href={encodeURI(`http://www.tumblr.com/`)}
+          >
+            {tumblricon}
+          </a>
+          <button
+            style={{ backgroundColor: color }}
+            id="new-quote"
+            onClick={() => getRandomQuotes()}
+          >
+            New Quote
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
